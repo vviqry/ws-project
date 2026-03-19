aa9e4a3e3f119779f5c9f0d8bf0cc6a48729f47f
Fikri <fikrymutsaqoful@gmail.com>
2026-03-19 22:32:08 +0700

    ws-project

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("node:http");
+const websocket = require("ws");
+const app = express();
+const server = http.createServer(app);
+const wss = new websocket.Server({ server });
+
+app.use(express.static(__dirname));
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === websocket.OPEN) {
+        client.send(data.toString());
+      }
+    });
+  });
+});
+
+server.listen(8080, function listening() {
+  console.log("Listening on %d", server.address().port);
+});
